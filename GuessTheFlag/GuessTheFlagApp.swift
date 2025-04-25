cbf260d6493949ed0786f55d38f54dfcbde18f18
Mohamed Obaya <mohamed.ali.obaya@gmail.com>
2025-04-25 11:56:34 +0300

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Mohamed Obaya on 25/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
